e931f8bc815a23c3685d062d1318163afc46ed7e
MuhamadFarrasSuhendi <farrazsuhendi2003@gmail.com>
2023-11-05 07:51:38 +0700

    Tugas UTS

---
+++ Admin Panel/resources/views/layout/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png" />
+    <link rel="icon" type="image/png" href="./assets/img/favicon.png" />
+    <title>Admin Group.F</title>
+   @include('layout.partial.link')
+  </head>
+
+  <body class="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
+    <div class="absolute w-full bg-blue-500 dark:hidden min-h-75"></div>
+    <x-header/>
+    <x-sidebar/>
+    
+
+      @yield('content')
+
+
+    @include('layout.partial.config') 
+  </body>
+  <x-footer/>
+  @include('layout.partial.script')
+</html>
